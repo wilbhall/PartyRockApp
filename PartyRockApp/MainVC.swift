7d4b204621e37352d78afce1dc2cc0834eb636b0
wilbhall <joseph.leichter@gmail.com>
2016-12-24 19:48:35 -0500

    Ex 62 UI done

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Joseph Leichter on 12/23/16.
+//  Copyright © 2016 Joseph Leichter. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
